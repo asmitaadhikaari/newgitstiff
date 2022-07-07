6439a473b880344f5d81a9e6a8738dc54a615852
asmitaadhikari <asmitaadhikari787@gmail.com>
2022-07-07 10:29:38 -0500

    upgraded

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -26,8 +26,10 @@ public class stepDefination {
     public void home_page_is_populated() throws Throwable {
         System.out.println("validated home page");
         System.out.println("validated home1111 page");
-
-
+        System.out.println("validated home1114 page");
+        System.out.println("validated home11133 page");
+        System.out.println("validated home11133 pageeee");
+        System.out.println("validated home1113344 page");
     }
 
     @When("^user login application with \"([^\"]*)\" and password \"([^\"]*)\"$")
@@ -40,6 +42,7 @@ public class stepDefination {
     @And("^cards are displayed$")
     public void cards_are_displayed() throws Throwable {
         System.out.println("validated cards");
+        System.out.println("validated cards1");
 
     }
 
